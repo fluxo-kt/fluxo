a07d55365c70668aa5dc11073d3813bc3ab2f55e
Artyom Shendrik <artyom.shendrik@gmail.com>
2022-12-08 22:26:33 +0400

    `IgnoreLinux` annotation for tests
    Signed-off-by: Artyom Shendrik <artyom.shendrik@gmail.com>

---
+++ fluxo-core/src/commonTest/kotlin/kt/fluxo/test/IgnoreLinux.kt
@@ -0,0 +1,9 @@
+package kt.fluxo.test
+
+/**
+ * Marks test or suite as ignored for Linux platform.
+ */
+@OptionalExpectation
+@OptIn(ExperimentalMultiplatform::class)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+expect annotation class IgnoreLinux()
---
+++ fluxo-core/src/linuxTest/kotlin/kt/fluxo/test/IgnoreLinux.kt
@@ -0,0 +1,4 @@
+package kt.fluxo.test
+
+@Suppress("ACTUAL_WITHOUT_EXPECT")
+actual typealias IgnoreLinux = kotlin.test.Ignore
