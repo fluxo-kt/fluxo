fbe87c5064bb2803dfd9b756860fdb5c48052f48
Artyom Shendrik <artyom.shendrik@gmail.com>
2023-01-07 17:32:08 +0400

    Adapt `PipelineInterceptionChain` for multiple intercepted actions
    Signed-off-by: Artyom Shendrik <artyom.shendrik@gmail.com>

--- benchmarks/jmh/src/main/kotlin/kt/fluxo/test/interception/PipelineInterceptionChain.kt
+++ benchmarks/jmh/src/main/kotlin/kt/fluxo/test/interception/PipelineInterceptionChain.kt
@@ -4,63 +4,71 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.runBlocking
 import kt.fluxo.test.compare.BENCHMARK_REPETITIONS
 import kt.fluxo.test.compare.CommonBenchmark.addAndGet
-import kt.fluxo.test.interception.PipelineInterceptionChain.Interceptor
+import kt.fluxo.test.interception.PipelineInterceptionChain.IntentInterceptor
 
+/**
+ * Pipeline interception as used in OkHttp, but adapted for multiple intercepted actions.
+ */
 object PipelineInterceptionChain {
 
     // https://square.github.io/okhttp/features/interceptors/
     // https://github.com/square/okhttp/blob/3ad1912/okhttp/src/jvmMain/kotlin/okhttp3/Interceptor.kt
 
-    private fun interface Interceptor {
-        suspend fun intercept(chain: Chain): Int
 
-        interface Chain {
-            val request: Int
+    private sealed interface Interceptor
 
-            suspend fun proceed(request: Int): Int
-        }
+    private fun interface IntentInterceptor<I> : Interceptor {
+        suspend fun interceptIntent(chain: InterceptorChain<I>): I
     }
 
-    private class RealInterceptorChain(
-        private val interceptors: List<Interceptor>,
+    private interface InterceptorChain<R> {
+        val value: R
+
+        suspend fun proceed(value: R): R
+    }
+
+    private abstract class RealInterceptorChain<R, I : Interceptor>(
+        @JvmField
+        protected val interceptors: Array<out I>,
         private val index: Int,
-        override val request: Int,
-    ) : Interceptor.Chain {
+        override val value: R,
+    ) : InterceptorChain<R> {
 
-        private var calls: Int = 0
+        protected abstract fun copy(index: Int = this.index, value: R = this.value): InterceptorChain<R>
 
-        private fun copy(index: Int = this.index, request: Int = this.request) = RealInterceptorChain(interceptors, index, request)
-
-        override suspend fun proceed(request: Int): Int {
+        override suspend fun proceed(value: R): R {
             check(index < interceptors.size) { "index ($index) >= interceptors.size (${interceptors.size})" }
 
-            calls++
-
             // Call the next interceptor in the chain.
-            val next = copy(index = index + 1, request = request)
+            val next = copy(index = index + 1, value = value)
             val interceptor = interceptors[index]
 
-            @Suppress("USELESS_ELVIS")
             return interceptor.intercept(next) ?: throw NullPointerException("interceptor $interceptor returned null")
         }
+
+        protected abstract suspend fun I.intercept(next: InterceptorChain<R>): R?
+    }
+
+    private class IntentInterceptorChain<I>(interceptors: Array<out IntentInterceptor<I>>, index: Int, value: I) :
+        RealInterceptorChain<I, IntentInterceptor<I>>(interceptors, index, value) {
+
+        override fun copy(index: Int, value: I) = IntentInterceptorChain(interceptors, index, value)
+
+        override suspend fun IntentInterceptor<I>.intercept(next: InterceptorChain<I>) = interceptIntent(next)
     }
 
     private suspend fun getResponseWithInterceptorChain(state: MutableStateFlow<Int>): Int {
-        val interceptor = Interceptor { it.proceed(it.request + state.addAndGet(1)) }
-        val finalInterceptor = Interceptor { it.request + state.addAndGet(1) }
+        val interceptor = IntentInterceptor<Int> { it.proceed(it.value + state.addAndGet(1)) }
+        val finalInterceptor = IntentInterceptor<Int> { it.value + state.addAndGet(1) }
 
         // Build a full stack of interceptors
 
         val interceptors = Array(INTERCEPTIONS_COUNT + 1) {
             if (it == INTERCEPTIONS_COUNT) finalInterceptor else interceptor
-        }.asList()
+        }
 
         val originalRequest = 0
-        val chain = RealInterceptorChain(
-            interceptors = interceptors,
-            index = 0,
-            request = originalRequest,
-        )
+        val chain = IntentInterceptorChain(interceptors, index = 0, originalRequest)
         return chain.proceed(originalRequest)
     }
 
