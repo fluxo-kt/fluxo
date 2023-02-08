04a24c7d7b438af9e68d848f768cda376c7ea67e
Artyom Shendrik <artyom.shendrik@gmail.com>
2023-02-08 21:51:41 +0400

    refactor(api)!: Clean up the `MigrationKt` api, fix compilation error in the `orbit` migration helper
    Signed-off-by: Artyom Shendrik <artyom.shendrik@gmail.com>

--- fluxo-core/src/commonMain/kotlin/kt/fluxo/core/dsl/Migration.kt
+++ fluxo-core/src/commonMain/kotlin/kt/fluxo/core/dsl/Migration.kt
@@ -8,40 +8,32 @@ import kt.fluxo.core.Container
 import kt.fluxo.core.FluxoIntent
 import kt.fluxo.core.FluxoSettings
 import kt.fluxo.core.Store
-import kt.fluxo.core.annotation.ExperimentalFluxoApi
 import kotlin.internal.InlineOnly
-import kotlin.jvm.JvmSynthetic
 
 @InlineOnly
-@Deprecated(message = "Please use the container instead", replaceWith = ReplaceWith("container"))
+@Deprecated(message = "Please use container instead", replaceWith = ReplaceWith("container"))
 public inline val <S, SE : Any> ContainerHost<S, SE>.store get() = container
 
 
 @InlineOnly
-@JvmSynthetic
-@ExperimentalFluxoApi
 @Deprecated(
-    message = "Please use the send instead",
+    message = "Please use send instead",
+    replaceWith = ReplaceWith("send(intent)"),
     level = DeprecationLevel.ERROR,
-    replaceWith = ReplaceWith("send(intent)"),
 )
-// TODO: inline modifier causes NPE in InlineParameterChecker
-public suspend fun <S, SE : Any> Container<S, SE>.orbit(intent: FluxoIntent<S, SE>) = sendAsync(intent)
+public suspend inline fun <S, SE : Any> Container<S, SE>.orbit(noinline intent: FluxoIntent<S, SE>) = sendAsync(intent)
 
 @InlineOnly
 @Deprecated(
-    message = "Please use the send instead",
+    message = "Please use send instead",
+    replaceWith = ReplaceWith("send(intent)"),
     level = DeprecationLevel.WARNING,
-    replaceWith = ReplaceWith("send(intent)"),
 )
-public inline fun <I, S, SE : Any> Store<I, S, SE>.accept(intent: I): Unit = send(intent)
+public inline fun <I, S, SE : Any> Store<I, S, SE>.accept(intent: I) = send(intent)
 
 
 @InlineOnly
-@Deprecated(
-    message = "Please use the intentContext instead",
-    replaceWith = ReplaceWith("intentContext"),
-)
+@Deprecated(message = "Please use intentContext instead", replaceWith = ReplaceWith("intentContext"))
 @OptIn(ExperimentalStdlibApi::class)
 public inline var <I, S, SE : Any> FluxoSettings<I, S, SE>.intentDispatcher: CoroutineDispatcher
     get() = intentContext[CoroutineDispatcher] ?: Dispatchers.Default
