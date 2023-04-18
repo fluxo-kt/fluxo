ac8c8a59fbbc4bdc1992c331c16e437a1036cea3
Artyom Shendrik <artyom.shendrik@gmail.com>
2023-04-18 19:21:34 +0200

    test(JobTest): fix assertions known to fail
    Signed-off-by: Artyom Shendrik <artyom.shendrik@gmail.com>

--- fluxo-core/src/commonTest/kotlin/kt/fluxo/tests/coroutines/JobTest.kt
+++ fluxo-core/src/commonTest/kotlin/kt/fluxo/tests/coroutines/JobTest.kt
@@ -5,9 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kt.fluxo.test.Platform
 import kt.fluxo.test.runUnitTest
-import kt.fluxo.test.testLog
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
 import kotlin.test.assertFalse
@@ -74,99 +72,61 @@ class JobTest {
             // NOTE: delay is safe bacause of Dispatcher
             delay(Long.MAX_VALUE)
         }
-        testLog("job_completion_with_children // childJob: $childJob")
-        testLog("job_completion_with_children // childJob.isActive: ${childJob.isActive}")
         assertTrue(childJob.isActive, "childJob.isActive 1")
-        testLog("job_completion_with_children // childJob.isCompleted: ${childJob.isCompleted}")
         assertFalse(childJob.isCompleted, "childJob.isCompleted 1")
-        testLog("job_completion_with_children // childJob.isCancelled: ${childJob.isCancelled}")
         assertFalse(childJob.isCancelled, "childJob.isCancelled 1")
         job.children.toList().let {
-            testLog("job_completion_with_children // job.children: $it")
-            assertContentEquals(listOf(childJob), it, "job.children, check 1")
+            assertContentEquals(listOf(childJob), it, "job.children, check 1: $it")
         }
 
-        job.complete().let {
-            testLog("job_completion_with_children // job.complete(): $it")
-            assertTrue(it)
-        }
-        testLog("job_completion_with_children // job.isActive: ${job.isActive}")
+        assertTrue(job.complete(), "job.complete() 2")
         assertTrue(job.isActive, "job.isActive 2")
-        testLog("job_completion_with_children // job.isCompleted: ${job.isCompleted}")
         assertFalse(job.isCompleted, "job.isCompleted 2")
-        testLog("job_completion_with_children // job.isCancelled: ${job.isCancelled}")
         assertFalse(job.isCancelled, "job.isCancelled 2")
         job.children.toList().let {
-            testLog("job_completion_with_children // job.children: $it")
-            assertContentEquals(listOf(childJob), it, "job.children, check 2")
+            assertContentEquals(listOf(childJob), it, "job.children, check 2: $it")
         }
 
-        testLog("job_completion_with_children // childJob.isActive: ${childJob.isActive}")
         assertTrue(childJob.isActive, "childJob.isActive 3")
-        testLog("job_completion_with_children // childJob.isActive: ${childJob.isCompleted}")
         assertFalse(childJob.isCompleted, "childJob.isCompleted 3")
-        testLog("job_completion_with_children // childJob.isActive: ${childJob.isCancelled}")
         assertFalse(childJob.isCancelled, "childJob.isCancelled 3")
 
         var wasCalled = false
         val childJob2 = launch(job) { wasCalled = true }
-        testLog("job_completion_with_children // childJob2.isActive: ${childJob2.isActive}")
         assertTrue(childJob2.isActive, "childJob2.isActive 4")
-        testLog("job_completion_with_children // childJob2.isCompleted: ${childJob2.isCompleted}")
         assertFalse(childJob2.isCompleted, "childJob2.isCompleted 4")
-        testLog("job_completion_with_children // childJob2.isCancelled: ${childJob2.isCancelled}")
         assertFalse(childJob2.isCancelled, "childJob2.isCancelled 4")
         job.children.toList().let {
-            testLog("job_completion_with_children // job.children: $it")
-            assertContentEquals(listOf(childJob, childJob2), it, "job.children, check 3")
+            assertContentEquals(listOf(childJob, childJob2), it, "job.children, check 3: $it")
         }
 
-        testLog("childJob.cancel()")
         childJob.cancel()
-        testLog("job_completion_with_children // childJob.isActive: ${childJob.isActive}")
         assertFalse(childJob.isActive, "childJob.isActive 5")
-        testLog("job_completion_with_children // childJob.isActive: ${childJob.isCompleted}")
-        assertFalse(childJob.isCompleted, "childJob.isCompleted 5")
-        testLog("job_completion_with_children // childJob.isActive: ${childJob.isCancelled}")
+        // childJob.isCompleted can have any value at this moment (jvm, linuxX64, iosX64, macosX64, tvosX64)
         assertTrue(childJob.isCancelled, "childJob.isCancelled 5")
 
-        testLog("job_completion_with_children // job.isActive: ${job.isActive}")
         assertTrue(job.isActive, "job.isActive 6")
-        testLog("job_completion_with_children // job.isCompleted: ${job.isCompleted}")
         assertFalse(job.isCompleted, "job.isCompleted 6")
-        testLog("job_completion_with_children // job.isCancelled: ${job.isCancelled}")
         assertFalse(job.isCancelled, "job.isCancelled 6")
         job.children.toList().let {
-            testLog("job_completion_with_children // job.children: $it")
-            assertContentEquals(listOf(childJob, childJob2), it, "job.children, check 4")
+            assertContentEquals(listOf(childJob, childJob2), it, "job.children, check 4: $it")
         }
 
-        testLog("job.join()")
         job.join()
-        testLog("job_completion_with_children // childJob.isActive: ${childJob.isActive}")
         assertFalse(childJob.isActive, "childJob.isActive 7")
-        testLog("job_completion_with_children // childJob.isCompleted: ${childJob.isCompleted}")
         assertTrue(childJob.isCompleted, "childJob.isCompleted 7")
-        testLog("job_completion_with_children // childJob.isCancelled: ${childJob.isCancelled}")
         assertTrue(childJob.isCancelled, "childJob.isCancelled 7")
 
         assertTrue(wasCalled, "Child job launched while in Completing state expected to be executed")
-        testLog("job_completion_with_children // childJob2.isActive: ${childJob2.isActive}")
         assertFalse(childJob2.isActive, "childJob2.isActive 8")
-        testLog("job_completion_with_children // childJob2.isCompleted: ${childJob2.isCompleted}")
         assertTrue(childJob2.isCompleted, "childJob2.isCompleted 8")
-        testLog("job_completion_with_children // childJob2.isCancelled: ${childJob2.isCancelled}")
         assertFalse(childJob2.isCancelled, "childJob2.isCancelled 8")
 
-        testLog("job_completion_with_children // job.isActive: ${job.isActive}")
         assertFalse(job.isActive, "job.isActive 9")
-        testLog("job_completion_with_children // job.isCompleted: ${job.isCompleted}")
         assertTrue(job.isCompleted, "job.isCompleted 9")
-        testLog("job_completion_with_children // job.isCancelled: ${job.isCancelled}")
         assertFalse(job.isCancelled, "job.isCancelled 9")
         job.children.toList().let {
-            testLog("job_completion_with_children // job.children: $it")
-            assertContentEquals(listOf(), it, "job.children, check 5")
+            assertContentEquals(listOf(), it, "job.children, check 5: $it")
         }
 
         job.cancel()
@@ -180,74 +140,48 @@ class JobTest {
             // NOTE: delay is safe bacause of Dispatcher
             delay(Long.MAX_VALUE)
         }
-        testLog("job_cancellation_with_children // childJob: $childJob")
-        testLog("job_cancellation_with_children // childJob.isActive: ${childJob.isActive}")
         assertTrue(childJob.isActive, "childJob.isActive 1")
-        testLog("job_cancellation_with_children // childJob.isCompleted: ${childJob.isCompleted}")
         assertFalse(childJob.isCompleted, "childJob.isCompleted 1")
-        testLog("job_cancellation_with_children // childJob.isCancelled: ${childJob.isCancelled}")
         assertFalse(childJob.isCancelled, "childJob.isCancelled 1")
         job.children.toList().let {
-            testLog("job_cancellation_with_children // job.children: $it")
-            assertContentEquals(listOf(childJob), it, "job.children, check 1")
+            assertContentEquals(listOf(childJob), it, "job.children, check 1: $it")
         }
 
         job.cancel()
-        testLog("job_cancellation_with_children // job.isActive: ${job.isActive}")
         assertFalse(job.isActive, "job.isActive 2")
-        testLog("job_cancellation_with_children // job.isCompleted: ${job.isCompleted}")
-        assertFalse(job.isCompleted, "job.isCompleted 2")
-        testLog("job_cancellation_with_children // job.isCancelled: ${job.isCancelled}")
+        // job.isCompleted can have any value at this moment (jvm, linuxX64, mingwX64, macosX64)
         assertTrue(job.isCancelled, "job.isCancelled 2")
 
-        testLog("job_cancellation_with_children // childJob.isActive: ${childJob.isActive}")
         assertFalse(childJob.isActive, "childJob.isActive 3")
-        testLog("job_cancellation_with_children // childJob.isCompleted: ${childJob.isCompleted}")
-        assertFalse(childJob.isCompleted, "childJob.isCompleted 3")
-        testLog("job_cancellation_with_children // childJob.isCancelled: ${childJob.isCancelled}")
+        // childJob.isCompleted can have any value at this moment (linuxX64, tvosX64, watchosX64)
         assertTrue(childJob.isCancelled, "childJob.isCancelled 3")
 
         val childJob2 = launch(job) {
             fail("Shouldn't be called!")
         }
-        testLog("job_cancellation_with_children // childJob2.isActive: ${childJob2.isActive}")
         assertFalse(childJob2.isActive, "childJob2.isActive 4")
-        testLog("job_cancellation_with_children // childJob2.isCompleted: ${childJob2.isCompleted}")
         assertFalse(childJob2.isCompleted, "childJob2.isCompleted 4")
-        testLog("job_cancellation_with_children // childJob2.isCancelled: ${childJob2.isCancelled}")
         assertTrue(childJob2.isCancelled, "childJob2.isCancelled 4")
-        val childrenList = job.children.toList()
         job.children.toList().let {
-            testLog("job_cancellation_with_children // job.children: $it")
-            if (!Platform.isNative || childrenList.isNotEmpty()) {
-                assertContentEquals(listOf(childJob), childrenList, "job.children, check 2")
+            if (it.isNotEmpty()) { // mingwX64, sometimes for JVM/Android too!
+                assertContentEquals(listOf(childJob), it, "job.children, check 2: $it")
             }
         }
 
         childJob.join()
-        testLog("job_cancellation_with_children // childJob.isActive: ${childJob.isActive}")
         assertFalse(childJob.isActive, "childJob.isActive 5")
-        testLog("job_cancellation_with_children // childJob.isCompleted: ${childJob.isCompleted}")
         assertTrue(childJob.isCompleted, "childJob.isCompleted 5")
-        testLog("job_cancellation_with_children // childJob.isCancelled: ${childJob.isCancelled}")
         assertTrue(childJob.isCancelled, "childJob.isCancelled 5")
 
-        testLog("job_cancellation_with_children // childJob2.isActive: ${childJob2.isActive}")
         assertFalse(childJob2.isActive, "childJob2.isActive 6")
-        testLog("job_cancellation_with_children // childJob2.isCompleted: ${childJob2.isCompleted}")
-        assertTrue(childJob2.isCompleted || Platform.isNative, "childJob2.isCompleted 6")
-        testLog("job_cancellation_with_children // childJob2.isCancelled: ${childJob2.isCancelled}")
+        // childJob2.isCompleted can have any value at this moment (Android, mingwX64)
         assertTrue(childJob2.isCancelled, "childJob2.isCancelled 6")
 
-        testLog("job_cancellation_with_children // job.isActive: ${job.isActive}")
         assertFalse(job.isActive, "job.isActive 7")
-        testLog("job_cancellation_with_children // job.isCompleted: ${job.isCompleted}")
         assertTrue(job.isCompleted, "job.isCompleted 7")
-        testLog("job_cancellation_with_children // job.isCancelled: ${job.isCancelled}")
         assertTrue(job.isCancelled, "job.isCancelled 7")
         job.children.toList().let {
-            testLog("job_cancellation_with_children // job.children: $it")
-            assertContentEquals(listOf(), it, "job.children, check 3")
+            assertContentEquals(listOf(), it, "job.children, check 3: $it")
         }
     }
 
