f05b8d50e58c3cabb35671b09e13b083522d87be
Artyom Shendrik <artyom.shendrik@gmail.com>
2023-04-22 17:51:19 +0200

    feat: draft of convenience DSL for StoreFactory
    Signed-off-by: Artyom Shendrik <artyom.shendrik@gmail.com>

--- fluxo-core/src/commonMain/kotlin/kt/fluxo/core/factory/StoreFactory.kt
+++ fluxo-core/src/commonMain/kotlin/kt/fluxo/core/factory/StoreFactory.kt
@@ -2,8 +2,13 @@
 
 package kt.fluxo.core.factory
 
+import kt.fluxo.core.Container
+import kt.fluxo.core.ContainerS
+import kt.fluxo.core.FluxoIntent
+import kt.fluxo.core.FluxoIntentS
 import kt.fluxo.core.FluxoSettings
 import kt.fluxo.core.IntentHandler
+import kt.fluxo.core.Reducer
 import kt.fluxo.core.SideEffectsStrategy
 import kt.fluxo.core.Store
 import kt.fluxo.core.StoreSE
@@ -12,7 +17,12 @@ import kt.fluxo.core.annotation.ThreadSafe
 import kt.fluxo.core.container
 import kt.fluxo.core.debug.DEBUG
 import kt.fluxo.core.debug.DebugStoreDecorator
+import kt.fluxo.core.internal.ReducerIntentHandler
 import kt.fluxo.core.store
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+import kotlin.internal.InlineOnly
+import kotlin.internal.LowPriorityInOverloadResolution
 import kotlin.js.JsName
 import kotlin.jvm.JvmName
 import kotlin.native.ObjCName
@@ -84,4 +94,121 @@ public abstract class StoreFactory {
         }
         return create<Intent, State, Nothing>(initialState, handler, settings)
     }
+
+
+    // region StoreFactory extensions for Store
+
+    /**
+     * Returns MVVM+ [Store], created with current [StoreFactory].
+     */
+    @InlineOnly
+    @JsName("container")
+    public inline fun <State> container(
+        initialState: State,
+        settings: FluxoSettings<FluxoIntentS<State>, State, Nothing>? = null,
+        @BuilderInference setup: FluxoSettings<FluxoIntentS<State>, State, Nothing>.() -> Unit = {},
+    ): ContainerS<State> {
+        contract { callsInPlace(setup, InvocationKind.EXACTLY_ONCE) }
+        return container(
+            initialState = initialState,
+            settings = settings,
+            factory = this,
+            setup = setup,
+        )
+    }
+
+    /**
+     * Returns MVVM+ [Store] with [SideEffect] support, created with current [StoreFactory].
+     */
+    @InlineOnly
+    @LowPriorityInOverloadResolution
+    @JsName("containerWithSideEffects")
+    @JvmName("containerWithSideEffects")
+    @ObjCName("containerWithSideEffects")
+    public inline fun <State, SideEffect : Any> container(
+        initialState: State,
+        settings: FluxoSettings<FluxoIntent<State, SideEffect>, State, SideEffect>? = null,
+        @BuilderInference setup: FluxoSettings<FluxoIntent<State, SideEffect>, State, SideEffect>.() -> Unit = {},
+    ): Container<State, SideEffect> {
+        contract { callsInPlace(setup, InvocationKind.EXACTLY_ONCE) }
+        return container(
+            initialState = initialState,
+            settings = settings,
+            factory = this,
+            setup = setup,
+        )
+    }
+
+    /**
+     * Returns basic [Reducer]-based [Store], created with current [StoreFactory].
+     *
+     * Use [container] for MVVM+ [Store].
+     */
+    @InlineOnly
+    @JsName("storeWithReducer")
+    public inline fun <Intent, State> store(
+        initialState: State,
+        @BuilderInference noinline reducer: Reducer<Intent, State>,
+        settings: FluxoSettings<Intent, State, Nothing>? = null,
+        @BuilderInference setup: FluxoSettings<Intent, State, Nothing>.() -> Unit = {},
+    ): Store<Intent, State> {
+        contract { callsInPlace(setup, InvocationKind.EXACTLY_ONCE) }
+        return store(
+            initialState = initialState,
+            handler = ReducerIntentHandler(reducer),
+            settings = settings,
+            factory = this,
+            setup = setup,
+        )
+    }
+
+    /**
+     * Returns MVI [Store] with MVVM+ [IntentHandler] DSL, created with current [StoreFactory].
+     *
+     * Use [container] for MVVM+ [Store].
+     */
+    @InlineOnly
+    @JsName("store")
+    public inline fun <Intent, State> store(
+        initialState: State,
+        @BuilderInference handler: IntentHandler<Intent, State, Nothing>,
+        settings: FluxoSettings<Intent, State, Nothing>? = null,
+        @BuilderInference setup: FluxoSettings<Intent, State, Nothing>.() -> Unit = {},
+    ): Store<Intent, State> {
+        contract { callsInPlace(setup, InvocationKind.EXACTLY_ONCE) }
+        return store(
+            initialState = initialState,
+            handler = handler,
+            settings = settings,
+            factory = this,
+            setup = setup,
+        )
+    }
+
+    /**
+     * Returns MVI [Store] with MVVM+ [IntentHandler] DSL and [SideEffect] support, created with current [StoreFactory].
+     *
+     * Use [container] for MVVM+ [Store].
+     */
+    @InlineOnly
+    @JsName("storeWithSideEffects")
+    @JvmName("storeWithSideEffects")
+    @ObjCName("storeWithSideEffects")
+    public inline fun <Intent, State, SideEffect : Any> store(
+        initialState: State,
+        @BuilderInference handler: IntentHandler<Intent, State, SideEffect>,
+        settings: FluxoSettings<Intent, State, SideEffect>? = null,
+        @BuilderInference setup: FluxoSettings<Intent, State, SideEffect>.() -> Unit = {},
+    ): StoreSE<Intent, State, SideEffect> {
+        contract { callsInPlace(setup, InvocationKind.EXACTLY_ONCE) }
+        return store(
+            initialState = initialState,
+            handler = handler,
+            settings = settings,
+            factory = this,
+            setup = setup,
+        )
+    }
+
+    // endregion
 }
