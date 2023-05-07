0990a16867a04a7d2635b8d6e5cc1499cacb306f
Artyom Shendrik <artyom.shendrik@gmail.com>
2023-05-07 13:14:05 +0200

    benchmark: update `IncrementIntentBenchmark` results and kdoc
    Signed-off-by: Artyom Shendrik <artyom.shendrik@gmail.com>

--- benchmarks/jmh/src/jmh/kotlin/kt/fluxo/jmh/IncrementIntentBenchmark.kt
+++ benchmarks/jmh/src/jmh/kotlin/kt/fluxo/jmh/IncrementIntentBenchmark.kt
@@ -20,29 +20,43 @@ import org.openjdk.jmh.infra.Blackhole
 /**
  * Benchmark for simple incrementing intent throughput.
  *
- * Don't try comparing dispatching here,
- * only maximum direct performance possible with state-container.
+ * Single-thread.
+ * No dispatching, only maximum direct performance possible with state-container.
+ *
+ * Each operation creates a state store, sends 5000 intents with reduction, and checks state updates.
  *
  * _Example results:_
  * ```
- * Benchmark                Mode  Cnt   Score    Error   Units  Percent
- * naive__state_flow       thrpt   18   9.533 ±  0.322  ops/ms    86.7%
- * mvicore__mvi_reducer    thrpt   18   5.107 ±  0.165  ops/ms     0.0%
- * fluxo__mvi_reducer      thrpt   18   3.772 ±  0.247  ops/ms   -26.1%
- * mvikotlin__mvi_reducer  thrpt   18   1.781 ±  0.063  ops/ms   -65.1%
- * fluxo__mvvmp_intent     thrpt   18   1.276 ±  0.164  ops/ms   -75.0%
- * fluxo__mvi_handler      thrpt   18   1.271 ±  0.100  ops/ms   -75.1%
- * ballast__mvi_handler    thrpt   18   0.389 ±  0.116  ops/ms   -92.4%
- * orbit__mvvmp_intent     thrpt   18   0.259 ±  0.007  ops/ms   -94.9%
+ * Benchmark                   Mode  Cnt   Score   Error   Units  Percent
+ * naive__state_flow          thrpt   12  11.235 ± 0.055  ops/ms    12.9%
+ * reduxkotlin__mvi_reducer   thrpt   12   9.949 ± 0.105  ops/ms     0.0%
+ * mvicore__mvi_reducer       thrpt   12   5.787 ± 0.029  ops/ms   -41.8%
+ * visualfsm__sm_reducer      thrpt   12   5.239 ± 0.037  ops/ms   -47.3%
+ * fluxo__mvi_reducer         thrpt   12   5.106 ± 0.089  ops/ms   -48.7%
+ * mvikotlin__mvi_reducer     thrpt   12   2.074 ± 0.049  ops/ms   -79.2%
+ * reduktor__mvi_reducer      thrpt   12   1.869 ± 0.021  ops/ms   -81.2%
+ * fluxo__mvvmp_intent        thrpt   12   1.457 ± 0.025  ops/ms   -85.4%
+ * genakureduce__mvi_handler  thrpt   12   1.443 ± 0.018  ops/ms   -85.5%
+ * fluxo__mvi_handler         thrpt   12   1.438 ± 0.036  ops/ms   -85.5%
+ * orbit__mvvmp_intent        thrpt   12   0.639 ± 0.021  ops/ms   -93.6%
+ * ballast__mvi_handler       thrpt   12   0.597 ± 0.016  ops/ms   -94.0%
+ * flowmvi__mvi_handler       thrpt   12   0.445 ± 0.013  ops/ms   -95.5%
+ * flowredux__mvi_handler     thrpt   12   0.112 ± 0.003  ops/ms   -98.9%
  *
- * naive__state_flow        avgt   18   0.390 ±  0.026   ms/op   -51.6%
- * mvicore__mvi_reducer     avgt   18   0.805 ±  0.046   ms/op     0.0%
- * fluxo__mvi_reducer       avgt   18   0.939 ±  0.041   ms/op    16.6%
- * mvikotlin__mvi_reducer   avgt   18   2.201 ±  0.066   ms/op   173.4%
- * fluxo__mvvmp_intent      avgt   18   3.222 ±  0.117   ms/op   300.2%
- * fluxo__mvi_handler       avgt   18   3.329 ±  0.192   ms/op   313.5%
- * ballast__mvi_handler     avgt   18   7.912 ±  0.365   ms/op   882.9%
- * orbit__mvvmp_intent      avgt   18  14.025 ±  0.478   ms/op  1642.2%
+ * naive__state_flow           avgt   12   0.260 ± 0.004   ms/op   -15.9%
+ * reduxkotlin__mvi_reducer    avgt   12   0.309 ± 0.004   ms/op     0.0%
+ * mvicore__mvi_reducer        avgt   12   0.510 ± 0.002   ms/op    65.0%
+ * visualfsm__sm_reducer       avgt   12   0.564 ± 0.003   ms/op    82.5%
+ * fluxo__mvi_reducer          avgt   12   0.612 ± 0.004   ms/op    98.1%
+ * mvikotlin__mvi_reducer      avgt   12   1.449 ± 0.070   ms/op   368.9%
+ * reduktor__mvi_reducer       avgt   12   1.593 ± 0.018   ms/op   415.5%
+ * fluxo__mvi_handler          avgt   12   1.960 ± 0.363   ms/op   534.3%
+ * fluxo__mvvmp_intent         avgt   12   2.051 ± 0.017   ms/op   563.8%
+ * genakureduce__mvi_handler   avgt   12   2.052 ± 0.023   ms/op   564.1%
+ * orbit__mvvmp_intent         avgt   12   4.590 ± 0.111   ms/op  1385.4%
+ * ballast__mvi_handler        avgt   12   5.088 ± 0.346   ms/op  1546.6%
+ * flowmvi__mvi_handler        avgt   12   6.611 ± 0.468   ms/op  2039.5%
+ * flowredux__mvi_handler      avgt   12  25.454 ± 0.459   ms/op  8137.5%
  * ```
  */
 @State(Scope.Benchmark)
