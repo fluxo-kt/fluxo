4b14303f8287e2fd595a7eec0beb91c5e66935a1
Artyom Shendrik <artyom.shendrik@gmail.com>
2023-05-07 20:39:58 +0200

    benchmark: add a benchmark and tests for the Elmslie
    See for details: https://github.com/vivid-money/elmslie
    
    Signed-off-by: Artyom Shendrik <artyom.shendrik@gmail.com>

---
+++ benchmarks/jmh/src/main/kotlin/kt/fluxo/test/compare/elmslie/ElmslieBenchmark.kt
@@ -0,0 +1,61 @@
+package kt.fluxo.test.compare.elmslie
+
+import kotlinx.coroutines.runBlocking
+import kt.fluxo.test.compare.BENCHMARK_ARG
+import kt.fluxo.test.compare.IntentAdd
+import kt.fluxo.test.compare.IntentIncrement
+import kt.fluxo.test.compare.consumeCommonBenchmark
+import kt.fluxo.test.compare.launchCommonBenchmark
+import kt.fluxo.test.compare.launchCommonBenchmarkWithStaticIntent
+import vivid.money.elmslie.core.store.ElmStore
+import vivid.money.elmslie.core.store.NoOpActor
+import vivid.money.elmslie.core.store.Result
+import vivid.money.elmslie.core.store.StateReducer
+import vivid.money.elmslie.coroutines.states
+
+internal object ElmslieBenchmark {
+
+    private fun <Event : Any> createStore(reducer: StateReducer<Event, Int, Nothing, Nothing>): ElmStore<Event, Int, Nothing, Nothing> {
+        return ElmStore(
+            initialState = 0,
+            reducer = reducer,
+            actor = NoOpActor()
+        ).start()
+    }
+
+    fun elmReducerStaticIncrement(): Int {
+        val store = createStore { event: IntentIncrement, state: Int ->
+            when (event) {
+                IntentIncrement.Increment -> {
+                    // Not enough information to infer type variable F
+                    @Suppress("RemoveExplicitTypeArguments")
+                    Result<Int, Nothing, Nothing>(state = state + 1)
+                }
+            }
+        }
+        runBlocking {
+            val launchDef = launchCommonBenchmarkWithStaticIntent(IntentIncrement.Increment) { store.accept(it) }
+            store.states.consumeCommonBenchmark(launchDef)
+            store.stop()
+        }
+        return store.currentState
+    }
+
+    fun elmReducerAdd(value: Int = BENCHMARK_ARG): Int {
+        val store = createStore { event: IntentAdd, state: Int ->
+            when (event) {
+                is IntentAdd.Add -> {
+                    // Not enough information to infer type variable F
+                    @Suppress("RemoveExplicitTypeArguments")
+                    Result<Int, Nothing, Nothing>(state + event.value)
+                }
+            }
+        }
+        runBlocking {
+            val launchDef = launchCommonBenchmark { store.accept(IntentAdd.Add(value = value)) }
+            store.states.consumeCommonBenchmark(launchDef)
+            store.stop()
+        }
+        return store.currentState
+    }
+}
